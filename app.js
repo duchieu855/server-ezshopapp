7d254713868d675464f2723f3865e572d4ca2dc5
duchieu <nguyenbaduchieu@gmail.com>
2024-03-18 17:47:00 +0700

    complete

---
+++ app.js
@@ -0,0 +1,170 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import { LowSync } from "lowdb";
+import { JSONFileSync } from "lowdb/node";
+import axios from "axios";
+
+const db = new LowSync(new JSONFileSync("database.json"), {
+	users: [],
+});
+
+// Initialize Express app
+const app = express();
+
+// Define a JWT secret key. This should be isolated by using env variables for security
+const jwtSecretKey = "dsfdsfsdfdsvcsvdfgefg";
+
+// Set up CORS and JSON middlewares
+const corsOptions = {
+	origin: "http://localhost:3000",
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Basic home route for the API
+app.get("/users", (_req, res) => {
+	res.send(db.data.user);
+});
+
+// The auth endpoint that creates a new user record or logs a user based on an existing record
+app.post("/auth", (req, res) => {
+	const { userName, password } = req.body;
+	// Look up the user entry in the database
+	const user = db.data.users.filter((user) => userName === user.userName);
+	console.log(user);
+	// If found, compare the hashed passwords and generate the JWT token for the user
+	if (user.length === 1) {
+		bcrypt.compare(password, user[0].password, function (_err, result) {
+			if (!result) {
+				return res.status(401).json({ message: "Invalid password" });
+			} else {
+				let loginData = {
+					userName,
+					signInTime: Date.now(),
+				};
+
+				const token = jwt.sign(loginData, jwtSecretKey);
+				res.status(200).json({ message: "success", token });
+			}
+		});
+		// If no user is found, hash the given password and create a new entry in the auth db with the userName and hashed password
+	} else {
+		return res.status(401).json({ message: "Invalid UserName" });
+	}
+});
+
+app.post("/register", (req, res) => {
+	const { userName, password } = req.body;
+
+	const indexUser = db.data.users.findIndex(
+		(user) => userName === user.userName
+	);
+
+	if (indexUser === -1) {
+		bcrypt.hash(password, 10, function (_err, hash) {
+			console.log({ userName, password: hash });
+			db.data.users.push({ userName, password: hash });
+
+			let loginData = {
+				userName,
+				signInTime: Date.now(),
+			};
+
+			const token = jwt.sign(loginData, jwtSecretKey);
+			res.status(200).json({ message: "success", token });
+		});
+	} else {
+		const user = db.data.users;
+		bcrypt.hash(password, 10, function (_err, hash) {
+			user[indexUser].password = hash;
+
+			let loginData = {
+				userName,
+				signInTime: Date.now(),
+			};
+
+			const token = jwt.sign(loginData, jwtSecretKey);
+			res.status(200).json({ message: "success", token });
+		});
+	}
+});
+
+// The verify endpoint that checks if a given JWT token is valid
+app.post("/verify", (req, res) => {
+	const tokenHeaderKey = "jwt-token";
+	const authToken = req.headers[tokenHeaderKey];
+	try {
+		const verified = jwt.verify(authToken, jwtSecretKey);
+		if (verified) {
+			return res.status(200).json({ status: "logged in", message: "success" });
+		} else {
+			// Access Denied
+			return res.status(401).json({ status: "invalid auth", message: "error" });
+		}
+	} catch (error) {
+		// Access Denied
+		return res.status(401).json({ status: "invalid auth", message: "error" });
+	}
+});
+
+// An endpoint to see if there's an existing account for a given userName address
+app.post("/check-account", (req, res) => {
+	const { userName } = req.body;
+
+	console.log(req.body);
+
+	const user = db.data.users.filter((user) => userName === user.userName);
+
+	console.log(user);
+	if (user.length === 0) {
+		res.status(200).json({
+			status: "User does not exist",
+		});
+	} else if (user.length === 1) {
+		res.status(200).json({
+			status: "User exists",
+		});
+	} else {
+		return res.status(401).json({ message: "UserName Exist" });
+	}
+});
+
+app.post("/authGoogle", async (req, res) => {
+	const { code } = req.body;
+	console.log("code", code);
+	const client_id =
+		"139470086134-ch811s3mb473es9kgjuvqu3i9jjmq789.apps.googleusercontent.com";
+	const client_secret = "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";
+	const redirect_uri = "http://localhost:3080";
+	const grant_type = "authorization_code";
+
+	try {
+		const res = await axios.post(
+			"https://oauth2.googleapis.com/token",
+			new URLSearchParams({
+				code,
+				client_id,
+				client_secret,
+				redirect_uri,
+				grant_type,
+			}),
+			{
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			}
+		);
+		// console.log("res gg", res);
+		// const token = res;
+		// res.status(200).json({ message: "success", token });
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.listen(3080);
